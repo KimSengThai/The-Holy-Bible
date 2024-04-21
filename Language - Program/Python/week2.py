0ce31458d12a95fed2a813e175c17223e721f908
kimsengthai <kimseng.thai@hotmail.com>
2024-04-21 21:38:11 +1000

    python

---
+++ Language - Program/Python/week2.py
@@ -0,0 +1,63 @@
+# Program that will ask 3 inputs:
+# side1, side2, side3
+
+# check if its scalene, isosceles, or equilateral
+
+# side1 = float(input("Input Side A: "))
+# side2 = float(input("Input Side B: "))
+# side3 = float(input("Input Side C: "))
+
+# if side1 == side2 == side3:
+#     print("equilateral")
+# elif side1 != side2 and side2 != side3 and side1 != side3:
+#     print("scalene")
+# else:
+#     print("isosceles")
+
+# We can also use "or" "and" to check for isosceles
+# float to convert string input to integer
+
+
+# Looping (While Loop, continue executing)
+# break -> will make the loop stop
+# continue -> will make the loop keep going, skip the one that doesnt match the statement
+# i start at 0 which is numbers[index]
+# loop will stop when i is >= to len(numbers)
+# print numbers[index] when numbers[index] is even number
+# else in loop -> after finishing the loop, else will executed, however break will not work as looping will stop forcefully!
+# numbers = [10, 20, 4, 8, 5, 16, 100]
+
+# i = 0
+# while i < len(numbers):
+#     if numbers[i] % 2 != 0:
+#         i += 1
+#         continue
+#     print(numbers[i])
+#     i += 1
+# else: 
+#     print("Looping is done!")
+
+# Looping (For Loop, discontinue executing, range function)
+# Below is excuting to leave if the number bigger than 10.
+# numbers = [10, 20, 4, 8, 5, 16, 100]
+# for num in numbers:
+#     if num > 10:
+#         continue
+#     print(num)
+# else:
+#     print("For Loop finish")
+
+# Range function
+# range(0, 5), if you want a range from 0 to 4. Second argument is a -1.
+# for num in range(0, 5):
+#     print(num)
+
+# Asking the user if they want to continue or stop, (Traiangle Problem in the above)
+# 37:04
+
+
+
+
+
+
+
